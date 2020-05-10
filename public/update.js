c7edfbad1a762c121adf36ccffab2bceb345bd31
gabrielyamato <gabriellopesyamato@gmail.com>
2020-05-10 04:21:21 -0300

    Add  Copy Button

--- public/update.js
+++ public/update.js
@@ -1,8 +1,8 @@
-let button  = document.getElementById("generate_button")
+let button_generate  = document.getElementById("generate_button")
 let text  = document.getElementById("reponse_text")
 let texthidden = document.getElementById("text_hidden")  
 
-button.addEventListener("click",function(){
+button_generate.addEventListener("click",function(){
     
     axios.get("/generate").then(function(response){
 
@@ -15,6 +15,23 @@ button.addEventListener("click",function(){
         text.innerText = result
 
     })   
+
+
+})
+
+let button_copy  = document.getElementById("copy_button")
+
+button_copy.addEventListener("click",function(){
+
+ 
+        const el = document.createElement('textarea');
+        el.value = texthidden.innerText;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+  
+
 
 
 })
